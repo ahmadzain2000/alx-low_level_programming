3f94df5b04c1157e8d2b1adbe4702845c7d6497b
ahmadzain2000 <ahmadzain200010@gmail.com>
2023-03-21 15:18:59 -0700

    s

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,24 @@
-#include <stdio.h>
+#include "holberton.h"
 
-int main() {
-  putchar('_');
-  putchar('p');
-  putchar('u');
-  putchar('t');
-  putchar('c');
-  putchar('h');
-  putchar('a');
-  putchar('r');
-  putchar('\n');
-  return 0;
+/**
+ * main - Print "Holberton" followed by a new line.
+ *
+ * Description: You are not allowed to include standard libraries.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char holberton[] = "Holberton";
+	int i = 0;
+
+	while (holberton[i] != '\0')
+	{
+		_putchar(holberton[i]);
+		i++;
+	}
+
+	_putchar('\n');
+
+	return (0);
 }
