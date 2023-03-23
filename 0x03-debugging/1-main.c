b4ff2e9463525dffaca064a2d27b9eb06488238c
ahmadzain2000 <ahmadzain200010@gmail.com>
2023-03-23 14:09:11 -0700

    ajk

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,19 +1,24 @@
-#include <stadio.h>
+#include <stdio.h>
+
 /**
- * main-case an infinite loop
- * Retrun:0
- */
-int main(vaiod)
+* main - causes an infinite loop
+* Return: 0
+*/
+
+int main(void)
 {
-int i ;
-printf("Infinite loop incoming :(/n");
-i= 0;
-/*
- * whil ( i < 10 )
- * {
- * putcher (i);
- * }
- */
-printf("Infinite loop avoided! \\o/\n");
- return (0);
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+	/*
+	*while (i < 10)
+	*{
+	*	putchar(i);
+	*}
+	*/
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
 }
